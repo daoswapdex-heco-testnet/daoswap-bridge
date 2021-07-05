b12e2ac13ae655eca9283deeca36b21e324ecae0
daoswap002 <daoswap@daoswap.cc>
2021-07-05 12:19:38 +0800

    init project

---
+++ src/views/common/Menu.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="menu">
+    <CLink href="https://app.heco.daoswap.cc/#/swap" class="item" target="_self">
+      {{ $t('common.menu.swap') }}
+    </CLink>
+    <CLink href="https://app.heco.daoswap.cc/#/pool" class="item" target="_self">
+      {{ $t('common.menu.pool') }}
+    </CLink>
+    <CLink href="https://app.heco.daoswap.cc/#/dao" class="item" target="_self">
+      {{ $t('common.menu.dao') }}
+    </CLink>
+    <CLink href="https://app.heco.daoswap.cc/#/governace" class="item" target="_self">
+      {{ $t('common.menu.governace') }}
+    </CLink>
+    <CLink
+      :to="{ name: 'home' }"
+      :class="$route.name == 'home' || $route.name == 'transactions' ? 'active item' : 'item'"
+      target="_self"
+    >
+      {{ $t('common.menu.token') }}
+    </CLink>
+    <!-- <CLink
+      :to="{ name: 'nft' }"
+      :class="$route.name == 'nft' ? 'active item' : 'item'"
+      target="_self"
+    >
+      {{ $t('common.menu.nft') }}
+    </CLink> -->
+  </div>
+</template>
+
+<script>
+import { TARGET_MAINNET } from '@/utils/env';
+
+export default {
+  name: 'Menu',
+  data() {
+    return {
+      mainnet: TARGET_MAINNET,
+    };
+  },
+  mounted() {},
+};
+</script>
+<style scoped lang="scss">
+.menu {
+  display: flex;
+  .item {
+    font-size: 14px;
+    font-family: PingFangTC-Medium, PingFangTC;
+    font-weight: 500;
+    color: rgba(255, 255, 255, 0.6);
+    line-height: 20px;
+    padding: 0 40px 0 0;
+    transition: all ease 0.3s;
+  }
+  .item:hover {
+    color: rgba(255, 255, 255, 1);
+    transition: all ease 0.3s;
+  }
+  .active {
+    color: rgba(255, 255, 255, 1) !important;
+  }
+}
+</style>
